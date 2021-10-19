e724633e48dcadfe7c53a971f6a1ab03daafa250
Marco Baldo <marco.baldo2@studio.unibo.it>
2021-10-19 16:08:40 +0200

    Update GCFRG_1st_report.r

--- GCFRG_1st_report.r
+++ GCFRG_1st_report.r
@@ -127,4 +127,4 @@ library (tinytex)         # librerie necessarie
 
 knitr::opts_chunk$set(warning = FALSE, message = FALSE, echo = FALSE)
 
-stitch("D:/iLand.r/GCFRG_1st_report.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))  
+stitch("D:/iLand.r/script_13.10_report.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))  
